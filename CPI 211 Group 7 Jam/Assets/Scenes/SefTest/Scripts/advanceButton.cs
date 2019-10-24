2c04b798a19280a4b279d6221f73a4fe31009292
itsFreakinSef <sefreif@gmail.com>
2019-10-23 22:49:29 -0700

    Sef Scene Updated + New Assets
    - Add Nav Mesh Components
    - Created sceneNav

---
+++ CPI 211 Group 7 Jam/Assets/Scenes/SefTest/Scripts/advanceButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class advanceButton : MonoBehaviour
+{
+    public GameObject trigger;
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(trigger);
+        Debug.Log("I'm Triggered!");
+    }
+}
---
+++ CPI 211 Group 7 Jam/Assets/Scenes/SefTest/Scripts/sceneNav.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class sceneNav : MonoBehaviour
+{
+    public bool toMove = true;
+    public GameObject[] waypoints;
+    int currentWaypoint = 0;
+
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            toMove = true;
+        }
+
+        if (waypoints.Length > 0 && toMove == true)
+        {
+            //moves to each waypoint
+            agent.SetDestination(waypoints[currentWaypoint].transform.position);
+
+            if (Vector3.Distance(waypoints[currentWaypoint].transform.position, transform.position) < 3.5)
+            {
+                currentWaypoint++;
+                toMove = false;
+                if (currentWaypoint >= waypoints.Length)
+                {
+                    currentWaypoint = 0;
+                }
+            }
+
+        }
+    }
+
+    public void MoveNext()
+    {
+        toMove = true;
+    }
+}
