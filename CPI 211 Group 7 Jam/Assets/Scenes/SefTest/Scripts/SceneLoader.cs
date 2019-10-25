a301d758113c7538c82ee67edf61450f4314bd46
itsFreakinSef <sefreif@gmail.com>
2019-10-25 12:38:59 -0700

    Menu Scenes
    - Added Start, Options, Game Scene into build settings
    - Created Victory + Game Over overlays (currently connected to "g" for Game Over and "v" for Victory

---
+++ CPI 211 Group 7 Jam/Assets/Scenes/SefTest/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Update()
+    {
+        Victory();
+        GameOver();
+    }
+
+    public void Menu()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Menu will be here");
+        }
+    }
+
+    public void Victory()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            SceneManager.LoadScene("VictoryScene", LoadSceneMode.Additive);
+        }
+    }
+
+    public void GameOver()
+    {
+        if(Input.GetKeyDown("g"))
+        {
+            SceneManager.LoadScene("LoseScene", LoadSceneMode.Additive);
+        }
+    }
+}
