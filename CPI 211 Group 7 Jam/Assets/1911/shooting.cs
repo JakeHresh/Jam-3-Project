9011198fba4872143a06f589f6cdc211a4afe271
TylerONE <yorukagekitsune@gmail.com>
2019-10-26 12:12:45 -0700

    added shield and gun animations

---
+++ CPI 211 Group 7 Jam/Assets/1911/shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("Fire");
+        }
+        if(Input.GetKeyUp("1"))
+        {
+            anim.Play("idle");
+        }
+    }
+}
+
+
---
+++ CPI 211 Group 7 Jam/Assets/BallisticShield/Shield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private bool toggle = false;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            if (toggle == true)
+            {
+                anim.Play("ShieldDown");
+                toggle = false;
+            }
+            else
+            {
+                anim.Play("ShieldUp");
+                toggle = true;
+            }
+        }
+    }
+}
