1f0eca484e1e07b8f45ca5872199caa8a48b1e95
TylerONE <yorukagekitsune@gmail.com>
2019-10-26 20:25:40 -0700

    got rid of unnecessary model files

--- CPI 211 Group 7 Jam/Assets/BallisticShield/Shield.cs
+++ CPI 211 Group 7 Jam/Assets/BallisticShield/Shield.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MonoBehaviour
-{
-    private bool toggle = false;
+{    private bool toggle = false;
     public Animator anim;
     // Start is called before the first frame update
     void Start()
