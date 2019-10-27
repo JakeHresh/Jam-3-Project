2f82520b8a2af81f9b5effef480c22a0cfd4e255
TylerONE <yorukagekitsune@gmail.com>
2019-10-26 21:04:36 -0700

    trying to fix my unity

--- CPI 211 Group 7 Jam/Assets/enemy/Enemy.cs
+++ CPI 211 Group 7 Jam/Assets/enemy/Enemy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject Player;
+    public Animator anim;
+    private float timeLeft = Random.Range(3.0f, 7.0f);
+    private float shootTime = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = new Vector3(Player.transform.position.x, this.transform.position.y, Player.transform.position.z);
+        this.transform.LookAt(targetPosition);
+
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            shoot();
+        }
     }
-}
+
+    private void shoot()
+    {
+        anim.SetBool("Shoot", true);
+        shootTime -= Time.deltaTime;
+        if (shootTime < 0)
+        {
+            anim.SetBool("Shoot", false);
+        }
+    }
+}   
