a00961e586ad85f41d2f2d5e622df829c013f22e
TylerONE <yorukagekitsune@gmail.com>
2019-10-26 22:04:27 -0700

    added all animations

--- CPI 211 Group 7 Jam/Assets/enemy/Enemy.cs
+++ CPI 211 Group 7 Jam/Assets/enemy/Enemy.cs
@@ -6,8 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public GameObject Player;
     public Animator anim;
-    private float timeLeft = Random.Range(3.0f, 7.0f);
-    private float shootTime = 2.0f;
+    private float timeLeft = 3;
+    private float shootTime = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +19,23 @@ public class Enemy : MonoBehaviour
     {
         Vector3 targetPosition = new Vector3(Player.transform.position.x, this.transform.position.y, Player.transform.position.z);
         this.transform.LookAt(targetPosition);
-
+        Debug.Log(anim.GetBool("shoot"));
         timeLeft -= Time.deltaTime;
         if (timeLeft < 0)
         {
             shoot();
         }
+        shootTime -= Time.deltaTime;
+        if (shootTime < 0 && anim.GetBool("shoot") == true)
+        {
+            anim.SetBool("shoot", false);
+            Debug.Log(anim.GetBool("shoot"));
+        }
     }
 
     private void shoot()
     {
-        anim.SetBool("Shoot", true);
-        shootTime -= Time.deltaTime;
-        if (shootTime < 0)
-        {
-            anim.SetBool("Shoot", false);
-        }
+        anim.SetBool("shoot", true);
+        Debug.Log(anim.GetBool("shoot"));
     }
 }   
